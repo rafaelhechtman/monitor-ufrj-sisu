def8a80366543296cd3de6fe55668ffa686130ac
rafaelhechtman <rafael.hechtman@liessin.g12.br>
2026-02-08 13:30:14 -0300

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -6,7 +6,7 @@ import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 
-URL = "https://acessograduacao.ufrj.br/periodo-2026-1/2026-sisu-mec"
+URL = "https://g1.globo.com/rj/rio-de-janeiro/"
 STATE_FILE = "estado_anterior.txt"
 
 EMAIL_FROM = os.getenv("EMAIL_FROM")
